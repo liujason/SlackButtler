51f08c82f22dc9c5a59f0a06af7eba951fa1e9a4
Jason Liu <liu.canada@gmail.com>
2016-02-25 23:30:51 -0500

    add coffee

--- hears-coffee.js
+++ hears-coffee.js
@@ -3,10 +3,22 @@ var setup = function(controller){
   // "coffee start 5m" (collect coffee list and end in 5 minutes.)
   // "coffee end"
 
-  controller.hears(['coffee'],'direct_message,mention', function(bot, msg){
-    bot.reply(msg, 'What would you like in your coffee sir?'+JSON.stringify(msg));
+  controller.hears(['coffee'],['ambient'], function(bot, message){
+    bot.startConversation(message, askSize);
   });
-
+  var askSize = function (response, convo){
+    convo.ask('What size would you like?', function(response, convo){
+      convo.say('(taking note -'+response.text+')');
+      askFlavor(response, convo);
+      convo.next();
+    });
+  };
+  var askFlavor = function (response, convo){
+    convo.ask ('What would you like in your coffee?', function(response, convo){
+      convo.say('got it, '+response.text);
+      convo.next();
+    });
+  };
 };
 
 module.exports = setup;
