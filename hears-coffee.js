80239b34ff0178cd6b79c5a79595e06bd4c7b4b7
Jason Liu <liu.canada@gmail.com>
2016-02-25 23:17:58 -0500

    add direct_message

--- hears-coffee.js
+++ hears-coffee.js
@@ -3,7 +3,7 @@ var setup = function(controller){
   // "coffee start 5m" (collect coffee list and end in 5 minutes.)
   // "coffee end"
 
-  controller.hears(['coffee'],'mention', function(bot, msg){
+  controller.hears(['coffee'],'direct_message,mention', function(bot, msg){
     bot.reply(message, 'What would you like in your coffee sir?'+JSON.stringify(message));
   });
 
